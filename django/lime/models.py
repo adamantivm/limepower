016dce7f83b92a906587312ac4c1f6dde4ffc4d1
Tessa Lau <tlau@willowgarage.com>
2013-03-31 16:42:20 -0700

    First pass at lime models

--- django/lime/models.py
+++ django/lime/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Relationship(models.Model):
+    """A connection between two (or more) people"""
+    pass
+
+class Person(models.Model):
+    """Half of a relationship"""
+    name = models.CharField(max_length=256, blank=False)
+    email = models.CharField(max_length=256, blank=False)
+    relationship = models.ForeignKey(Relationship)
