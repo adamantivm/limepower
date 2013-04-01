190468483aa815fb568adb693e88f07249407311
Tessa Lau <tlau@willowgarage.com>
2013-03-31 17:48:16 -0700

    Add status and nicknames to person model

--- django/lime/models.py
+++ django/lime/models.py
@@ -10,4 +10,15 @@ class Person(models.Model):
     """Half of a relationship"""
     name = models.CharField(max_length=256, blank=False)
     email = models.CharField(max_length=256, blank=False)
+    
+    # We assume that each person is in at most one relationship
+    # We assume (though do not enforce) that a relationship contains only
+    # two people
     relationship = models.ForeignKey(Relationship)
+
+    # Nickname for the other person in the relationship
+    nickname = models.CharField(max_length=256, blank=False)
+
+    # This person's online status
+    status = models.BooleanField()
+    last_updated = models.DateTimeField(auto_now=True)
