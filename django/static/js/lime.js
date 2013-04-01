94c5ded7936bc7ea7b6eaa513f11f9dc34a05be6
Tessa Lau <tlau@willowgarage.com>
2013-03-31 22:16:10 -0700

    Add heart display

--- django/static/js/lime.js
+++ django/static/js/lime.js
@@ -6,9 +6,10 @@ $('document').ready(function(){
     sock.emit('connected',1);
   });
   sock.on('disconnect', resetLimes);
-  $('#my-status').on('click',function() {
+  $('#heart').on('click',function() {
     newval = ($('#my-status').html() == 'online') ? 0 : 1;
     updateLime( 'my-status', newval ? 'online': 'offline');
+    updateHeart();
     sock.emit('changed', newval);
   });
 });
@@ -26,8 +27,25 @@ function updateLimes( limeValues) {
   ['her-status','my-status'].map(function(lime){
     updateLime( lime, limeValues[lime] ? 'online' : 'offline');
   });
+  updateHeart();
 }
 
 function updateLime( whichLime, limeText) {
   $('#'+whichLime).html( limeText);
+}
+
+function updateHeart() {
+  var me = $('#my-status').html() == 'online';
+  var them = $('#her-status').html() == 'online';
+
+  var img = $('#heart');
+  if (me && them) {
+    img.attr('src', 'img/heart-full.png'); 
+  } else if (me) {
+    img.attr('src', 'img/heart-left.png'); 
+  } else if (them) {
+    img.attr('src', 'img/heart-right.png'); 
+  } else {
+    img.attr('src', 'img/heart-outline.png'); 
+  }
 }
